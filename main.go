7005205b6c76916d8b8151e446ce18c5237189fa
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2024-05-08 19:19:56 -0300

    arquivo main com endpoint get

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Forte Senha Function")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
