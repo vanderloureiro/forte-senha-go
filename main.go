ad106ae261dd0505e1dbe6b172ec78c400f553af
Vanderlei Loureiro <vanderloureiroleite@gmai.com>
2024-05-16 21:30:33 -0300

    selecao aleatoria de palavras em array

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"math/rand"
 )
 
 func main() {
 
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Forte Senha Function")
+		fmt.Fprint(w, generatePassword())
 	})
 
 	http.ListenAndServe(":8080", nil)
+}
+
+var words = [10]string{"feijao", "bicicleta", "churrasco", "sanduiche", "helicoptero", "geladeira", "ensolarado", "frigideira", "ornitorrinco", "intercambio"}
+
+func generatePassword() string {
+	randIndex := rand.Intn(10)
+	return words[randIndex]
 }
